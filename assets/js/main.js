66ecde2f0c5b4980634cd0d67d42b74c53ad6a8f
Lienzomobiliario <lienzomobiliarioweb@gmail.com>
2021-02-13 11:34:38 -0500

    adding staticforms
    Connecting contact form with staticForms

--- assets/js/main.js
+++ assets/js/main.js
@@ -429,44 +429,6 @@
             e.preventDefault();
         });
 
-        /*===============================================================
-         Working Contact Form
-         ================================================================*/
-
-        $("#contactForm").submit(function(e) {
-
-            e.preventDefault();
-            var $ = jQuery;
-
-            var postData = $(this).serializeArray(),
-                formURL = $(this).attr("action"),
-                $cfResponse = $('#contactFormResponse'),
-                $cfsubmit = $("#cfsubmit"),
-                cfsubmitText = $cfsubmit.text();
-
-            $cfsubmit.text("Sending...");
-
-
-            $.ajax({
-                url: formURL,
-                type: "POST",
-                data: postData,
-                success: function(data) {
-                    $cfResponse.html(data);
-                    $cfsubmit.text(cfsubmitText);
-                    $('#contactForm input[name=name]').val('');
-                    $('#contactForm input[name=email]').val('');
-                    $('#contactForm textarea[name=message]').val('');
-                },
-                error: function(data) {
-                    alert("Error! Please try again");
-                }
-            });
-
-            return false;
-
-        });
-
 
         /*===============================================================
          Working Request A Call Form
